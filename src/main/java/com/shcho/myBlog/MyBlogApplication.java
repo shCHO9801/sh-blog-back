5157b9ede1ff18cd88777ae2b781220fd143bce8
sunghyun <csh980116@naver.com>
2026-01-15 20:27:07 +0900

    [Feat] Swagger(OpenAPI) 기반 API 문서화 및 인증 연동

--- src/main/java/com/shcho/myBlog/MyBlogApplication.java
+++ src/main/java/com/shcho/myBlog/MyBlogApplication.java
@@ -2,8 +2,10 @@ package com.shcho.myBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyBlogApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/shcho/myBlog/common/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.shcho.myBlog.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ShBlog API",
+                version = "v1",
+                description = "개인 블로그 프로젝트 백엔드 API 명세"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
