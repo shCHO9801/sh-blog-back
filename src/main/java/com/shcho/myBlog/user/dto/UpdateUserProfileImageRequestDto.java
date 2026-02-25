d1bb492157f3bc16ec3c8192cbde1b6b30d0285a
sunghyun <csh980116@naver.com>
2026-02-25 22:43:18 +0900

    [Feat] 마이페이지 - 내 정보 수정 기능 구현

--- src/main/java/com/shcho/myBlog/user/dto/UpdateUserProfileImageRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.shcho.myBlog.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.URL;
-
-public record UpdateUserProfileImageRequestDto(
-        @NotBlank @URL String profileImageUrl
-) {
-}
